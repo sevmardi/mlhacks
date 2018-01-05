21d87b1997490ee0a3db9287c5b8b224b1099ec9
Sevak Mardirosian <smardirosians@gmail.com>
2018-01-05 10:39:33 +0100

    few other implmentations

---
+++ AdalineGD.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class AdalineGD(object):
+    """Adaptive linear Neuron classifier"""
+
+    def __init__(self, eta=0.01, n_iter=50, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+
+    def fit(self, X, y):
+        """Fit training data"""
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors * 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def partial_fix(self, X, Y):
+        """Fit training data without re-init the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+
+        return self
+
+    def shuffle(self, X, y):
+        """Shuffle training data"""
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        """Initialize weights to zeros"""
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+    def update_weights(self, xi, target):
+        """Apply Adaline learning rule to update the weights"""
+        output = self.net_input(xi)
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error**2
+        return cost
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return self.net_input(X)
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+
+fix, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+adal = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(adal.cost_) + 1), np.log10(adal.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+plt.show()
--- perceptron.py
+++ perceptron.py
@@ -1,63 +1,61 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 class Perceptron(object):
-    """Perceptron classifier
-
-    Parameters
-    ----------
-    eta : float
-            Learning rate (between 0.0 and 1.0)
-    n_iter : int 
-            Passes over the training dataset. 
-
-    Attributes 
-    ----------
-    w_ : 1d-array 
-            Weights after fitting
-    errors_ : list 
-            Number of misclassifications in every epoch 
-    """
+    """Perceptron classifier """
 
     def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
 
     def fit(self, X, y):
-        """Fit training data
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors = []
 
-        Parameters 
-        -----------
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
 
-        X : {array-like}, shape = [n_samples, n_features]
-        	Training vectors, where n_samples is the number of samples and 
-        	n_features is the number of features 
-        y: arrray-like, shape=[n_samples] 
-        	Target values
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors.append(errors)
 
-        Returns
-        -------
-        self : object 
-        """
-		self.w_ = np.zeros(1 + X.shape[1])
-		self.errors_ []
+        return self
 
-		for _ in range(self.n_iter):
-			errors = 0
-			for xi, target in zip(X, y):
-				update = self.eta * (target - self.predict(xi))
-				self.w_[1:] += update * xi
-				self.w_[0] += update 
-				errors += int(update != 0.0)
-			self.errors_.append(errors)
-		return self
+    def net_input(self, X):
+        """ Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
 
-		def net_input(self, X):
-			""" Calculate net input"""
-			return np.dot(X, self.w_[1:]) + self.w_[0]
+    def predict(self, X):
+        """Return class label after unit step """
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
 
-		def predict(self, X):
-			"""Return class label after unit step """
-			return np.where(self.net_input(X) >= 0.0, 1, -1)
 
-		
+if __name__ == '__main__':
+    
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+ 
+    y = df.iloc[0:100, 4].values
+    y = np.where('y' == 'Iris-setosa', -1, 1)
+    X = df.iloc[0:100, [0,2]].values
+
+    ppn = Perceptron(eta=0.1, n_iter=10)
+    ppn.fit(X, y)
+    plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+    plt.xlabel('Epochs')
+    plt.ylabel("Number of misscalcullation")
+    plt.show()
+
+    # plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+    # plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicoor')
+    # plt.xlabel('petal length')
+    # plt.ylabel('sepal length')
+    # plt.legend(loc='upper left')
+
+    # plt.show()
